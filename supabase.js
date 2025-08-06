effbf30b80bb4d35df6805b8e87a414f787ac8c8
SOUMAINE RAMADAN <gess.srh.td@gmail.com>
2025-08-06 13:51:58 +0100

    Ajout des pages admin et utilisateur

--- supabase.js
+++ supabase.js
@@ -1,16 +1,14 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
-
 const SUPABASE_URL = 'https://mtjkijmzzycymjmdlvn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10amtpam16eXpjeXltam1kbHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyMDgwNzQsImV4cCI6MjA2OTc4NDA3NH0.s35vs0dVC0XrKMwPLgP_WxVUBZKuJRAbF6S0-dspTt0';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+// 'supabase' est déjà une variable globale après l'inclusion du script CDN
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// Vous pouvez également ajouter ici les fonctions d'authentification ou d'autres fonctions liées à Supabase
-// par exemple pour l'authentification par email et mot de passe
+// Vous pouvez ajouter d'autres fonctions ici si nécessaire
 // export async function signInWithEmail(email, password) {
-//     const { data, error } = await supabase.auth.signInWithPassword({
-//         email,
-//         password,
-//     });
-//     return { data, error };
+//      const { data, error } = await supabase.auth.signInWithPassword({
+//          email,
+//          password,
+//      });
+//      return { data, error };
 // }
